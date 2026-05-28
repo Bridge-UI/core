becfca98b028a1e0151180ad015625e2199aae11
João Pedro Lopes <joaopalopes24@gmail.com>
2026-05-28 11:25:08 -0300

    wip

--- packages/react/src/Components/FormField/FormField.tsx
+++ packages/react/src/Components/FormField/FormField.tsx
@@ -1,3 +1,7 @@
+// ** External Imports
+import { get } from "es-toolkit/compat";
+import { createElement } from "react";
+
 // ** Local Imports
 import FilledFormField from "@/Components/FormField/FilledFormField";
 import NotchedFormField from "@/Components/FormField/NotchedFormField";
@@ -6,32 +10,28 @@ import StackedFormField from "@/Components/FormField/StackedFormField";
 import UnderlinedFormField from "@/Components/FormField/UnderlinedFormField";
 import type { FormFieldProps } from "@/Components/FormField/formField.types";
 import type { UseFormFieldReturn } from "@/Components/FormField/hooks/useFormField";
+import { derived } from "@/Utils";
+
+const shells = {
+  filled: FilledFormField,
+  notched: NotchedFormField,
+  stacked: StackedFormField,
+  outline: OutlinedFormField,
+  underlined: UnderlinedFormField,
+};
 
 function FormField(
   props: Required<Pick<FormFieldProps, "field" | "children">>,
 ) {
   const api = props.field as UseFormFieldReturn;
 
-  const variant = api?.variantKey ?? "outline";
+  const Shell = derived(() => {
+    const variant = api?.variantKey ?? "outline";
 
-  switch (variant) {
-    case "filled":
-      return <FilledFormField api={api}>{props.children}</FilledFormField>;
+    return get(shells, variant, OutlinedFormField);
+  });
 
-    case "underlined":
-      return (
-        <UnderlinedFormField api={api}>{props.children}</UnderlinedFormField>
-      );
-
-    case "stacked":
-      return <StackedFormField api={api}>{props.children}</StackedFormField>;
-
-    case "notched":
-      return <NotchedFormField api={api}>{props.children}</NotchedFormField>;
-
-    case "outline":
-      return <OutlinedFormField api={api}>{props.children}</OutlinedFormField>;
-  }
+  return createElement(Shell, { api }, props.children);
 }
 
 export default FormField;
