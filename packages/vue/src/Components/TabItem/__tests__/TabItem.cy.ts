11798221f1b84ac5ed67dd955972cc10efe2b73c
João Pedro Lopes <joaopalopes24@gmail.com>
2026-07-28 18:27:10 -0300

    test(tab-item): fixing tab-item tests

--- packages/vue/src/Components/TabItem/__tests__/TabItem.cy.ts
+++ packages/vue/src/Components/TabItem/__tests__/TabItem.cy.ts
@@ -1,16 +1,19 @@
+// ** External Imports
+import { h } from "vue";
+
 // ** Local Imports
 import { TabItem } from "@/Components/TabItem";
 import { Tabs } from "@/Components/Tabs";
 
 test("it should render TabItem-driven tabs in the browser", () => {
-  cy.mount({
-    components: { Tabs, TabItem },
-    template: `
-      <Tabs model-value="a">
-        <TabItem label="A" value="a">Panel A</TabItem>
-        <TabItem label="B" value="b">Panel B</TabItem>
-      </Tabs>
-    `,
+  cy.mount(Tabs, {
+    props: { modelValue: "a" },
+    slots: {
+      default: () => [
+        h(TabItem, { label: "A", value: "a" }, { default: () => "Panel A" }),
+        h(TabItem, { label: "B", value: "b" }, { default: () => "Panel B" }),
+      ],
+    },
   });
 
   cy.get('[role="tablist"]').should("exist");
